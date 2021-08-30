1646b469d830273a504f9d6767298b092ccf47d5
harvinde <harvindergeek@gmail.com>
2021-08-30 14:35:10 +0530

    replaced the color library

--- src/plugins/text/index.ts
+++ src/plugins/text/index.ts
@@ -1,3 +1,7 @@
+import '@simonwep/pickr/dist/themes/monolith.min.css'; 
+import Pickr from '@simonwep/pickr';
+
+
 let name = 'Text';
 
 
@@ -54,10 +58,7 @@ let hideText = () => {
         const colorPicker = document.createElement("button");
         colorPicker.innerText ='C:';
         colorPicker.classList.add('colorPicker')
-        let colorPickerscript = document.createElement('script');
-        colorPickerscript.src = 'https://rawgithub.com/suffick/Picker/master/dist/picker.min.js';
-        document.head.appendChild(colorPickerscript);
-
+        
         
 
         
@@ -223,11 +224,11 @@ let hideText = () => {
     selectionNode.querySelectorAll(".colorPicker").forEach((item: any)=>{
        item.addEventListener("click", ()=>{
        console.log("clicked on color picker")
+       let parent = document.querySelector(".colorPicker");
 
-
-       let parent = document.querySelector(".colorPicker"); 
+      /* let parent = document.querySelector(".colorPicker"); 
         
-       let picker = new Picker({parent: (parent), orientation: ( 'center'), x:(-300), y: (-100)});
+       let picker: Picker = new Picker({parent: (parent), orientation: ( 'center'), x:(-300), y: (-100)});
 
         
        
@@ -241,10 +242,55 @@ let hideText = () => {
        
         
         picker.show();
-  
-       
-       
+        */
+       const pickr = Pickr.create({
+       el: '.colorPicker',
+       theme: 'monolith',
+       useAsButton: true,
 
+       swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+       ],
+
+      components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+           }
+       }
+     }).on('save', (color:any) => {
+      (<HTMLButtonElement>parent).style.background= color.toRGBA().toString(0);
+      (<HTMLTextAreaElement>document.querySelector(".minima-textbox")).style.color = color.toRGBA().toString();
+      pickr.hide();
+    });
+      console.log(pickr.getSelectedColor().toRGBA().toString(0));
+      console.log((<HTMLButtonElement>parent).style.background)
     });
   })
 
